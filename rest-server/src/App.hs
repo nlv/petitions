b0cb9a3283ab4a27239e2f722f4af9329a0d9a05
Leon Nikitin <nlv@lab321.ru>
2018-11-23 17:43:01 +0600

    rest: получение петиций sample

--- rest-server/src/App.hs
+++ rest-server/src/App.hs
@@ -2,11 +2,13 @@
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module App where
 
 import           Control.Monad.Trans.Except
 import           Data.Aeson
+import           Data.Text
 import           GHC.Generics
 import           Network.Wai
 import           Network.Wai.Handler.Warp
@@ -15,12 +17,14 @@ import           System.IO
 
 -- * api
 
-type ItemApi =
-  "item" :> Get '[JSON] [Item] :<|>
-  "item" :> Capture "itemId" Integer :> Get '[JSON] Item
+type Api = PetitionApi -- :<|> SingerApi
 
-itemApi :: Proxy ItemApi
-itemApi = Proxy
+type PetitionApi = 
+  "petition" :> Capture "id" Integer :> QueryParam "locale" Text :> Get '[JSON] Petition
+
+
+api :: Proxy Api
+api = Proxy
 
 -- * app
 
@@ -34,35 +38,65 @@ run = do
   runSettings settings =<< mkApp
 
 mkApp :: IO Application
-mkApp = return $ serve itemApi server
+mkApp = return $ serve api server
 
-server :: Server ItemApi
+server :: Server Api
 server =
-  getItems :<|>
-  getItemById
+  getPetitionById 
 
-getItems :: Handler [Item]
-getItems = return [exampleItem]
 
-getItemById :: Integer -> Handler Item
-getItemById = \ case
-  0 -> return exampleItem
+getPetitionById :: Integer -> Maybe Text -> Handler Petition
+getPetitionById i l = case (i, l) of
+  (0, Just "ru") -> return example0Ru
+  (0, Just "en") -> return example0En
+  (1, Nothing)   -> return example1
   _ -> throwE err404
 
-exampleItem :: Item
-exampleItem = Item 0 "example item"
+example0Ru :: Petition
+example0Ru = Petition 0 "Ноль" "Очень кратко" "Длииииииииинно" "ru"
 
--- * item
+example0En :: Petition
+example0En = Petition 0 "Zerro" "Very short" "Veeeeeeeryyyyyy Looooooong" "en"
 
-data Item
-  = Item {
-    itemId :: Integer,
+example1 :: Petition
+example1 = Petition 1 "One" "One petition" "One petition of the first petition" "en"
+
+-- * data
+
+data Petition
+  = Petition {
+    petitionId                :: Integer,
+    petitionName              :: Text,
+    petitionShortDescription  :: Text,
+    petitionDescription       :: Text,
+    petitionLocale            :: Text
+  }
+  deriving (Eq, Show, Generic)
+
+instance ToJSON Petition
+instance FromJSON Petition
+
+{-
+data Signer
+  = Signer {
+    petitionFirstName :: String,
+    petitionLastName  :: String,
+    petitionCountry :: String,
+    petitionOrganization :: Maybe Integer,
+    petitionEmail :: Integer,
+    petitionPhone :: Integer,
+    petitionBirthYear :: Integer,
+    petitionGender :: Integer,
+    petitionNotifiesEnabled :: Integer,
+    petition :: Integer,
+    petition :: Integer,
     itemText :: String
   }
   deriving (Eq, Show, Generic)
 
-instance ToJSON Item
-instance FromJSON Item
+instance ToJSON Signer
+instance FromJSON Signer
+-}
 
 data a + b = Foo a b
 
