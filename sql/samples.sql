5e993afa8561efddc51d695f95a125ba1a1273f5
Leon Nikitin <nlv@lab321.ru>
2018-11-23 16:21:01 +0600

    Скрипты по созданию бд

---
+++ sql/samples.sql
@@ -0,0 +1,52 @@
+INSERT INTO locales
+VALUES
+('ru'),
+('en'),
+('de')
+;
+
+INSERT INTO petitions
+(name, description, locale)
+VALUES 
+('За все хорошее!', 'Надо добиться улучшение жизни НАРОДА!!!!!!', 'ru'),
+('STOP CARS!', 'STOP USING CARS!!!!!!!', 'en')
+;
+
+INSERT INTO petitions_locale
+(petition_id, locale, name, description)
+VALUES
+((SELECT id FROM petitions WHERE name = 'За все хорошее!'), 'en', 'For all goods', 'We have done it'),
+((SELECT id FROM petitions WHERE name = 'STOP CARS!'), 'ru', 'Стоп машинам!', 'Ходить пешком!'),
+((SELECT id FROM petitions WHERE name = 'STOP CARS!'), 'de', 'Ahtung! STOP AVTO!', 'Shprihen ze dojch!')
+;
+
+INSERT INTO signers
+(
+    petition_id, 
+    first_name, 
+    last_name, 
+    country, 
+    organization, 
+    email, 
+    phone,
+    birth_year, 
+    gender, 
+    notifies_enabled,
+    ip,
+    inet_domain
+)
+VALUES
+(
+    (SELECT id FROM petitions WHERE name = 'За все хорошее!'), 
+    'Петр', 
+    'Петров', 
+    'Россия',
+    'ООО ППП',
+    'nlv@lab.ru',
+    '9139133322',
+    '1972',
+    'M',
+    TRUE,
+    '193.168.6.6',
+    'rbc.ru'
+);
